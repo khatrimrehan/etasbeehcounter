76518fafa4676b0613a13cce8cc97299472d1954
RK <khatrimrehan@gmail.com>
2025-12-10 14:46:51 +0530

    hahaha

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+let changeMode = document.getElementById("changemode")
+let lighT = document.getElementById("light")
+let loGo = document.getElementById("logo")
+let rSetBtn=document.getElementById("reset-btn")
+let finGer=document.getElementById("fingeR")
+
 function lightmode() {
-    console.log("hehehe")
+
+ loGo.src = "sparkel-dark.png"
+
+ rSetBtn.src = "reset-dark.png"
+ 
+ finGer.src = "finger-black.svg"
+
+ var NAME = document.getElementById("changemode")
+ 
+ NAME.className="lightmode"
+}
+
+function darkmode() {
+ loGo.src = "fluent--sparkle-16-regular (1).png"
+
+ rSetBtn.src = "resetlogo.png"
+ 
+  finGer.src = 
+"streamline--one-finger-tap-remix.svg" 
+ 
+ var NAME = document.getElementById("changemode")
+ 
+ NAME.className="lightmod"
 }
