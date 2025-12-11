71a289310b55c58acf773c6d2bf9c2ffe34672e5
RK <khatrimrehan@gmail.com>
2025-12-11 15:14:31 +0530

    hahaha

--- script.js
+++ script.js
@@ -7,70 +7,58 @@ let cliCked = document.getElementById("clicked")
 let cliCcked = document.getElementsByClassName("finger-text")
 let minUs = document.getElementById("hahaha")
 let incrimenTAnywhere = document.getElementById("incanywhere")
+let AnyWhere = document.getElementById("anywhere")
 
 
-const ignore = [rSetBtn, finGer, loGo, lighT, cliCked, minUs, cliCcked, incrimenTAnywhere];
+const ignoreElements = [rSetBtn, finGer, loGo, lighT, cliCked, minUs, incrimenTAnywhere, AnyWhere].filter(Boolean);
+const ignoreClassElements = Array.from(cliCcked || []).filter(Boolean);
 
-document.addEventListener("click", (e) => {
-  if (ignore.includes(e.target)) {
-    return;
-  }
-});
 
-function vibrate() {
-    if (navigator.vibrate) {
-        navigator.vibrate(40);
-    }
+function countany() {
+  console.log("hahahaha")
 }
 
+function vibrate() {
+  if (navigator.vibrate) {
+    navigator.vibrate(40);
+  }
+}
+function lightmode() {
+  loGo.src = "sparkel-dark.png"
+  rSetBtn.src = "reset-dark.png"
+  finGer.src = "finger-black.svg"
+  var NAME = document.getElementById("changemode")
+  NAME.className = "lightmode"
+}
+function darkmode() {
+  loGo.src = "fluent--sparkle-16-regular (1).png"
+  rSetBtn.src = "resetlogo.png"
+  finGer.src =
+    "streamline--one-finger-tap-remix.svg"
+  var NAME = document.getElementById("changemode")
+  NAME.className = "lightmod"
+}
 
+let count = 0
 
+let countEL = document.getElementById("count-el")
+
+function incrimentAnywhere() {
+  count += 1
+  countEL.textContent = count
+}
 function minuses() {
   if (count > 0) {
     count -= 1
   }
   countEL.textContent = count
 }
-
-
-
-
-function lightmode() {
-
-  loGo.src = "sparkel-dark.png"
-  rSetBtn.src = "reset-dark.png"
-  finGer.src = "finger-black.svg"
-
-  var NAME = document.getElementById("changemode")
-  NAME.className = "lightmode"
-}
-
-function darkmode() {
-  loGo.src = "fluent--sparkle-16-regular (1).png"
-  rSetBtn.src = "resetlogo.png"
-  finGer.src =
-    "streamline--one-finger-tap-remix.svg"
-
-  var NAME = document.getElementById("changemode")
-  NAME.className = "lightmod"
-}
-
-let count = 0
-let countEL = document.getElementById("count-el")
-
-
-
-
-function incrimentAnywhere() {
-  count += 1
-  countEL.textContent = count
-}
-
-
-
 function lineoNe() {
   countEL.textContent = 0
-  count = -1
+  count = 0
 }
+
+
+
 
 
