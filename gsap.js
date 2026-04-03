ea4e0c51c553f720be22d6badb05e740df3b57b5
RK <khatrimrehan@gmail.com>
2026-04-04 01:49:41 +0530

    heha

---
+++ gsap.js
@@ -0,0 +1,30 @@
+let hasVisited = localStorage.getItem("visited");
+
+if (!hasVisited) {
+
+  // 👉 FIRST TIME animation chalega
+  gsap.fromTo(".container", { autoAlpha: 1 }, {
+    autoAlpha: 0,
+    duration: 0.5,
+    delay: 8,
+  });
+
+  gsap.fromTo(".logo-name", {
+    y: 50,
+    autoAlpha: 0,
+  }, {
+    y: 0,
+    autoAlpha: 1,
+    duration: 2,
+    delay: 0.5,
+  });
+
+  // 👉 yaad rakh liya ke user aa gaya
+  localStorage.setItem("visited", "true");
+
+} else {
+
+  // 👉 refresh pe animation skip
+  document.querySelector(".container").style.display = "none";
+
+}
---
+++ script.js
@@ -0,0 +1,147 @@
+let changeMode = document.getElementById("changemode")
+let lighT = document.getElementById("light")
+let loGo = document.getElementById("logo")
+let rSetBtn = document.getElementById("reset-btn")
+let finGer = document.getElementById("fingeR")
+let cliCked = document.getElementById("clicked")
+let cliCcked = document.getElementsByClassName("finger-text")
+let minUs = document.getElementById("hahaha")
+let incrimenTAnywhere = document.getElementById("incanywhere")
+let AnyWhere = document.getElementById("anywhere")
+let menuBar = document.getElementById("menubar")
+let lightThemeLogo = document.getElementById("lightthemelogo")
+let linEoNe = document.getElementById("reset-btn")
+let aboutUs = document.getElementById("about-us")
+let backArrow = document.getElementById("backarrow")
+
+
+const ignoreElements = [rSetBtn, finGer, loGo, lighT, cliCked, minUs, incrimenTAnywhere, menuBar, AnyWhere].filter(Boolean);
+const ignoreClassElements = Array.from(cliCcked || []).filter(Boolean);
+
+
+
+
+function vibrate() {
+  if (navigator.vibrate) {
+    console.log("Supported: " + navigator.vibrate([100]));
+  } else {
+    console.log("Vibration NOT supported");
+  }
+  if (navigator.vibrate) {
+    navigator.vibrate(50);
+  }
+}
+function lightmode() {
+
+  if (isOpen) {
+    loGo.src = "sparkel-dark.png"
+    rSetBtn.src = "reset-dark.png"
+    finGer.src = "finger-black.svg"
+    lightThemeLogo.src = "sun.png"
+    lighT.textContent = "Dark Theme : Off"
+    var NAME = document.getElementById("changemode")
+    NAME.className = "lightmode"
+  }
+  else {
+    loGo.src = "fluent--sparkle-16-regular (1).png"
+    rSetBtn.src = "resetlogo.png"
+    finGer.src =
+      "streamline--one-finger-tap-remix.svg"
+    lightThemeLogo.src = "moon.png"
+
+    lighT.textContent = "Dark Theme : On"
+
+    var NAME = document.getElementById("changemode")
+    NAME.className = "lightmod"
+  }
+
+  isOpen = !isOpen
+}
+
+let count = 0
+
+let countEL = document.getElementById("count-el")
+
+let savedCount = localStorage.getItem("tasbeehCount");
+
+if (savedCount !== null) {
+  count = parseInt(savedCount);
+  countEL.textContent = count;
+}
+
+function incrimentAnywhere() {
+  count += 1
+  countEL.textContent = count
+
+  localStorage.setItem("tasbeehCount", count);
+}
+
+function countany() {
+  count += 1
+  countEL.textContent = count
+
+  localStorage.setItem("tasbeehCount", count);
+}
+
+
+function minuses() {
+  if (count > 0) {
+    count -= 1
+  }
+
+  countEL.textContent = count
+  localStorage.setItem("tasbeehCount", count);
+}
+
+
+
+
+function lineoNe() {
+  count = 0
+  countEL.textContent = 0
+
+  localStorage.setItem("tasbeehCount", count);
+
+  linEoNe.style.transform += "rotate(360deg)"
+}
+
+
+
+let isOpen = false;
+let isOpn = false;
+
+
+function menubtn() {
+
+  if (isOpen) {
+    menuBar.style.transform = "translate(120%) scale(0.75)";
+    menuBar.style.display = "flex"
+  } else {
+    menuBar.style.transform = "translate(0%)";
+
+  }
+
+  isOpen = !isOpen;
+
+
+}
+
+function about() {
+  menuBar.style.transform = "translate(120%) scale(0.75)";
+
+  aboutUs.style.transform = "translate(0%)"
+
+  
+}
+function back() {
+
+
+
+  menuBar.style.transform = "translate(0%)";
+  aboutUs.style.transform = "translate(-130%) scale(0.75)"
+}
+
+function Home() {
+  aboutUs.style.transform = "translate(-130%) scale(0.75)"
+  menuBar.style.transform = "translate(120%) scale(0.75)";
+}
